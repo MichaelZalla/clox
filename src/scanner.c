b12c81d91d2b78c2d2a546810633c5814bf6bdf3
Michael Zalla <michael@zalla.io>
2024-08-24 19:13:27 -0700

    16.4: Identifiers and Keywords;

--- src/scanner.c
+++ src/scanner.c
@@ -129,6 +129,103 @@ static Token number()
 	return makeToken(TOKEN_NUMBER);
 }
 
+static bool isAlpha(char c)
+{
+	return (c >= 'a' && c <= 'z') ||
+				 (c >= 'A' && c <= 'Z') ||
+				 (c == '_');
+}
+
+static TokenType checkKeyword(
+		int fromIndex,
+		int length,
+		const char *suffix,
+		TokenType type)
+{
+	// Checks length.
+	if (scanner.current - scanner.start == fromIndex + length)
+	{
+		// Matches `start[fromIndex : fromIndex+length]` against `suffix`.
+		int signDifference = memcmp(scanner.start + fromIndex, suffix, length);
+
+		if (signDifference == 0)
+		{
+			return type;
+		}
+	}
+
+	return TOKEN_IDENTIFIER;
+}
+
+static TokenType identifierType()
+{
+	switch (scanner.start[0])
+	{
+	case 'a':
+		return checkKeyword(1, 2, "nd", TOKEN_AND);
+	case 'c':
+		return checkKeyword(1, 4, "lass", TOKEN_CLASS);
+	case 'e':
+		return checkKeyword(1, 3, "lse", TOKEN_ELSE);
+	case 'f':
+		// Checks if we can read ahead by 1 character.
+		if (scanner.current - scanner.start > 1)
+		{
+			switch (scanner.start[1])
+			{
+			case 'a':
+				return checkKeyword(2, 3, "lse", TOKEN_FALSE);
+			case 'o':
+				return checkKeyword(2, 1, "r", TOKEN_FOR);
+			case 'u':
+				return checkKeyword(2, 1, "n", TOKEN_FUN);
+			}
+		}
+		break;
+	case 'i':
+		return checkKeyword(1, 1, "f", TOKEN_IF);
+	case 'n':
+		return checkKeyword(1, 2, "il", TOKEN_NIL);
+	case 'o':
+		return checkKeyword(1, 1, "r", TOKEN_OR);
+	case 'p':
+		return checkKeyword(1, 4, "rint", TOKEN_PRINT);
+	case 'r':
+		return checkKeyword(1, 5, "eturn", TOKEN_RETURN);
+	case 's':
+		return checkKeyword(1, 4, "uper", TOKEN_SUPER);
+	case 't':
+		// Checks if we can read ahead by 1 character.
+		if (scanner.current - scanner.start > 1)
+		{
+			switch (scanner.start[1])
+			{
+			case 'h':
+				return checkKeyword(2, 2, "is", TOKEN_THIS);
+			case 'r':
+				return checkKeyword(2, 2, "ue", TOKEN_TRUE);
+			}
+		}
+		break;
+	case 'v':
+		return checkKeyword(1, 2, "ar", TOKEN_VAR);
+	case 'w':
+		return checkKeyword(1, 4, "hile", TOKEN_WHILE);
+	}
+
+	return TOKEN_IDENTIFIER;
+}
+
+static Token identifier()
+{
+	// We know `isAlpha()` must be true for the initial character.
+
+	while (isAlpha(peek()) || isDigit(peek()))
+		advance();
+
+	return makeToken(identifierType());
+}
+
 static Token string()
 {
 	while (peek() != '"' && !isAtEnd())
@@ -158,6 +255,9 @@ Token scanToken()
 		return makeToken(TOKEN_EOF);
 
 	char c = advance();
+
+	if (isAlpha(c))
+		return identifier();
 
 	if (isDigit(c))
 		return number();
