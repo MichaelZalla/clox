590aa2a700195928d50ddbf471ecf635ab929e2a
Michael Zalla <michael@zalla.io>
2025-01-18 19:24:41 -0800

    17.2: Parsing Tokens;

--- src/compiler.c
+++ src/compiler.c
@@ -5,13 +5,100 @@
 #include "compiler.h"
 #include "scanner.h"
 
+typedef struct
+{
+	Token currentToken;
+	Token previousToken;
+	bool hadError;
+	bool panicMode;
+} Parser;
+
+Parser parser;
+
+static void errorAt(Token *token, const char *message)
+{
+	if (parser.panicMode)
+	{
+		return;
+	}
+
+	parser.panicMode = true;
+
+	fprintf(stderr, "[line %d] Error", token->line);
+
+	if (token->type == TOKEN_EOF)
+	{
+		fprintf(stderr, " at end");
+	}
+	else if (token->type == TOKEN_ERROR)
+	{
+		// Print nothing (as an ERROR token has no lexeme associated).
+	}
+	else
+	{
+		// Print the token's lexeme.
+		fprintf(stderr, " at '%.*s'", token->length, token->start);
+	}
+
+	fprintf(stderr, ": %s\n", message);
+
+	parser.hadError = true;
+}
+
+static void error(const char *message)
+{
+	errorAt(&parser.previousToken, message);
+}
+
+static void errorAtCurrent(const char *message)
+{
+	errorAt(&parser.currentToken, message);
+}
+
+static void advance()
+{
+	// Cache the (previously) current token.
+	parser.previousToken = parser.currentToken;
+
+	// Consumes the next token, and loops through any resulting errors.
+	for (;;)
+	{
+		parser.currentToken = scanToken();
+
+		if (parser.currentToken.type != TOKEN_ERROR)
+		{
+			break;
+		}
+
+		// Informs the user that the scanner encountered a scanning error.
+		errorAtCurrent(parser.currentToken.start);
+	}
+}
+
+static void consume(TokenType type, const char *message)
+{
+	if (parser.currentToken.type == type)
+	{
+		advance();
+
+		return;
+	}
+
+	errorAtCurrent(message);
+}
+
 bool compile(const char *source, Chunk *chunk)
 {
 	initScanner(source);
+
+	parser.hadError = false;
+	parser.panicMode = false;
 
 	advance();
 
 	expression();
 
 	consume(TOKEN_EOF, "Expect end of expression.");
+
+	return !parser.hadError;
 }
